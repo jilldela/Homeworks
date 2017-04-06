937c2f60627d03beda2416c90da93d8f14aac8de
jilldela <jilldelosangeles@gmail.com>
2017-04-05 18:06:39 -0700

    W2D3 dessert files

---
+++ W2D4/big_octopus.rb
@@ -0,0 +1,36 @@
+class Octopus
+
+  FISH = [
+    'fish',
+    'fiiish',
+    'fiiiiish',
+    'fiiiish',
+    'fffish',
+    'ffiiiiisshh',
+    'fsh',
+    'fiiiissshhhhhh'
+  ]
+
+  TILES = [
+    "up",
+    "right-up",
+    "right",
+    "right-down",
+    "down",
+    "left-down",
+    "left",
+    "left-up"
+  ]
+
+  def sluggish
+    # find the longest fish in O(n^2)
+  end
+
+  def dominant
+    # find the longest fish in O(n log n) #merge_sort
+  end
+
+  def clever
+    # find the longest fish in O(n)
+  end
+end
